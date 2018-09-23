f38e2e8f37fd9dfdc7cad1067921c42c406dfc94
Tagwa-Esam <tagwaesam5@gmail.com>
2018-09-23 18:59:02 -0400

    Solution

---
+++ app/scripts/legoBuilder.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name legoBuilder
+ * @description
+ * # legoBuilder
+ *
+ * Main module of the application.
+ */
+angular
+  .module('legoBuilder',[ ]);
+
+angular
+  .module('legoBuilder');
